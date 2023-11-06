f2143a9f02cffba95ddcb6e2bd586a2949516008
Podral3 <Podral3@DESKTOP-NR4OPI7>
2023-11-06 23:36:13 +0100

    Dodaj pliki projektów.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Filter = "(*.jpg)|*.jpg"
+        };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(ofd.FileName);
+                var engine = new TesseractEngine($"C:\\Users\\Podral3\\Desktop\\tessdata", "eng", EngineMode.Default);
+                string bruh = engine.Process(bmp).GetText();
+                this.textBox1.Text = bruh;
+            }
+        }
+    }
+}
